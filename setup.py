c7cc172569985c1f2dde37fc67bf2990ebd6c749
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-04-17 18:02:13 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+version_number = '0.0'
+
+
+setup(name='mkidcalculator',
+      description='Tools for analyzing MKID data',
+      version=version_number,
+      author='Nicholas Zobrist',
+      license='GPLv3',
+      url='http://github.com/zobristnicholas/mkidcalculator',
+      packages=find_packages(),
+      long_description=open('README.rst').read(),
+      install_requires=['numpy',
+                        'scipy'],
+      classifiers=['Development Status :: 1 - Planning',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering :: Physics'])
