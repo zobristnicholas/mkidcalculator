95aae66e5d00a4340b2901d8947c881065b2dceb
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-04-19 13:22:30 -0700

    doc string updates

--- mkidcalculator/noise.py
+++ mkidcalculator/noise.py
@@ -7,28 +7,51 @@ log.addHandler(logging.NullHandler())
 
 
 class Noise:
+    """A class for manipulating the noise data."""
     def __init__(self):
         self._data = AnalogReadoutNoise()  # dummy class replaced by load()
         log.info("Noise object created. ID: {}".format(id(self)))
 
     @property
     def f_bias(self):
+        """The bias frequency for the data set."""
         return self._data["f_bias"]
 
     @property
     def i_trace(self):
+        """The mixer I output traces."""
         return self._data["i_trace"]
 
     @property
     def q_trace(self):
+        """The mixer Q output traces."""
         return self._data["q_trace"]
 
     @property
     def metadata(self):
+        """A dictionary containing metadata about the pulse."""
         return self._data["metadata"]
 
     @classmethod
     def load(cls, file_name, data=AnalogReadoutNoise, **kwargs):
+        """
+        Noise class factory method that returns a Noise() with the data loaded.
+        Args:
+            file_name: string
+                The file name for the noise data.
+            data: object
+                Class or function whose return value allows dictionary-like
+                queries of the attributes required by the Noise class. The
+                default is the AnalogReadoutNoise class, which interfaces
+                with the data products from the analogreadout module.
+            kwargs: optional keyword arguments
+                extra keyword arguments are sent to 'data'. This is useful in
+                the case of the AnalogReadout* data classes for picking the
+                channel index.
+        Returns:
+            noise: object
+                A Noise() object containing the loaded data.
+        """
         noise = cls()
         noise._data = data(file_name, **kwargs)
         return noise
--- mkidcalculator/pulse.py
+++ mkidcalculator/pulse.py
@@ -7,35 +7,64 @@ log.addHandler(logging.NullHandler())
 
 
 class Pulse:
+    """A class for manipulating the pulse data."""
     def __init__(self):
         self._data = AnalogReadoutPulse()  # dummy class replaced by load()
         log.info("Pulse object created. ID: {}".format(id(self)))
 
     @property
     def f_bias(self):
+        """The bias frequency for the data set."""
         return self._data["f_bias"]
 
     @property
     def i_trace(self):
+        """The mixer I output traces."""
         return self._data["i_trace"]
 
     @property
     def q_trace(self):
+        """The mixer Q output traces."""
         return self._data["q_trace"]
 
     @property
     def offset(self):
+        """The mixer IQ offset at the bias frequency."""
         return self._data["offset"]
 
     @property
     def metadata(self):
+        """A dictionary containing metadata about the pulse."""
         return self._data["metadata"]
+
+    @property
+    def energies(self):
+        """The known photon energies in this data set."""
+        return self._data["energies"]
 
     @classmethod
     def load(cls, file_name, data=AnalogReadoutPulse, **kwargs):
+        """
+        Pulse class factory method that returns a Pulse() with the data loaded.
+        Args:
+            file_name: string
+                The file name for the pulse data.
+            data: object
+                Class or function whose return value allows dictionary-like
+                queries of the attributes required by the Pulse class. The
+                default is the AnalogReadoutPulse class, which interfaces
+                with the data products from the analogreadout module.
+            kwargs: optional keyword arguments
+                extra keyword arguments are sent to 'data'. This is useful in
+                the case of the AnalogReadout* data classes for picking the
+                channel index.
+        Returns:
+            pulse: object
+                A Pulse() object containing the loaded data.
+        """
         pulse = cls()
         pulse._data = data(file_name, **kwargs)
         return pulse
 
-    def compute_energies(self):
+    def compute_photon_energies(self):
         raise NotImplementedError
