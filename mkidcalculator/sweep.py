8151616565ac544c0b090268d697377bfb1d739b
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-04-23 17:00:24 -0700

    added temperature_groups for Sweep objects

--- mkidcalculator/sweep.py
+++ mkidcalculator/sweep.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 from operator import itemgetter
+from scipy.cluster.vq import kmeans2, ClusterError
 
 from mkidcalculator.loop import Loop
 from mkidcalculator.data import analogreadout_sweep
@@ -8,9 +10,68 @@ from mkidcalculator.data import analogreadout_sweep
 class Sweep:
     def __init__(self):
         self.loops = []
-        self.temperatures = []
         self.powers = []
         self.fields = []
+        self.temperatures = []
+        self.temperature_groups = []
+        # analysis results
+        self.loop_parameters = {}
+
+    def create_parameters(self, label="best", fit_type="lmfit", group=True, n_groups=None):
+        # check inputs
+        if fit_type not in ['lmfit', 'emcee', 'emcee_mle']:
+            raise ValueError("'fit_type' must be either 'lmfit', 'emcee', or 'emcee_mle'")
+        # group temperatures
+        if group:
+            temperatures = np.array(self.temperatures)
+            if n_groups is None:
+                n_groups = temperatures.size // (np.unique(self.powers).size * np.unique(self.fields).size)
+            k = np.linspace(temperatures.min(), temperatures.max(), n_groups)
+            try:
+                centroids, label = kmeans2(temperatures, k=k, minit='matrix', missing='raise')
+            except ClusterError:
+                message = "The temperature data is too disordered to cluster into {} groups".format(n_groups)
+                raise ClusterError(message)
+            self.temperature_groups = np.empty(temperatures.shape)
+            for index, centroid in enumerate(centroids):
+                self.temperature_groups[label == index] = centroid
+            self.temperature_groups = list(self.temperature_groups)
+        else:
+            self.temperature_groups = self.temperatures
+        # determine the parameter names
+        parameter_names = set()
+        parameters = []
+        for loop in self.loops:
+            if fit_type == "lmfit" and label in loop.lmfit_results.keys():  # only include lmfit
+                p = loop.lmfit_results[label]['result'].params.valuesdict()
+            elif fit_type == "emcee" and label in loop.emcee_results.keys():  # only include emcee
+                p = loop.emcee_results[label]['median']
+            elif fit_type == "emcee_mle" and label in loop.emcee_results.keys():  # only include emcee mle values
+                p = loop.emcee_results[label]['mle']
+            else:
+                p = None
+            # save the parameters and collect all the names into the set
+            parameters.append(p)
+            if p is not None:
+                for name in p.keys():
+                    parameter_names.add(name)
+        # initialize the data frame
+        parameter_names = sorted(list(parameter_names))
+        if group:
+            if "temperature" in parameter_names:
+                raise ValueError("'temperature' can not be a fit parameter name if group=True")
+            parameter_names.append("temperature")
+        indices = zip(self.powers, self.fields, self.temperature_groups)
+        multi_index = pd.MultiIndex.from_tuples(indices, names=["power", "field", "temperature group"])
+        df = pd.DataFrame(np.nan, index=multi_index, columns=parameter_names)
+        # fill the data frame
+        for index, loop in enumerate(self.loops):
+            if parameters[index] is not None:
+                for key, value in parameters[index].items():
+                    df.loc[indices[index]][key] = value
+            if group:
+                df.loc[indices[index]]["temperature"] = self.temperatures[index]
+        self.loop_parameters[label] = df
 
     def save(self):
         raise NotImplementedError
