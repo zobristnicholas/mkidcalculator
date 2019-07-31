8f1619387eae4bea8ef8b8c3ece61dc2dafb1f27
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-07-31 14:40:51 -0700

    first pass at qi model

--- mkidcalculator/models/__init__.py
+++ mkidcalculator/models/__init__.py
@@ -1,2 +1,3 @@
 from mkidcalculator.models.fr import Fr
+from mkidcalculator.models.qi import Qi
 from mkidcalculator.models.s21 import S21
---
+++ mkidcalculator/models/qi.py
@@ -0,0 +1,111 @@
+import numpy as np
+import lmfit as lm
+import scipy.constants as sc
+from mkidcalculator.models.utils import scaled_alpha, scaled_alpha_inv
+
+try:
+    from superconductivity import complex_conductivity as cc
+    HAS_SUPERCONDUCTIVITY = True
+except ImportError:
+    HAS_SUPERCONDUCTIVITY = False
+
+# constants
+pi = sc.pi
+kb = sc.k  # [J / K] Boltzmann const
+h = sc.h  # [J * s] Plank constant
+BCS = pi / np.exp(np.euler_gamma)  # bcs constant
+
+
+class Qi:
+    @classmethod
+    def mattis_bardeen(cls, params, temperatures, low_energy=False, parallel=False):
+        if not HAS_SUPERCONDUCTIVITY:
+            raise ImportError("The superconductivity package is not installed.")
+        if temperatures is None:
+            return 0.
+        # unpack parameters
+        alpha = params['alpha'].value
+        tc = params['tc'].value
+        bcs = params['bcs'].value
+        gamma = params['gamma'].value
+        limit = params['limit'].value
+        f0 = params['f0'].value
+        # calculate Qinv
+        sigma0 = cc.value(0, f0, tc, gamma=gamma, bcs=bcs, low_energy=low_energy, parallel=parallel)
+        sigma1 = cc.value(temperatures, f0, tc, gamma=gamma, bcs=bcs, low_energy=low_energy, parallel=parallel)
+        q_inv = alpha * limit * np.real((sigma1 - sigma0) * sigma0**(limit - 1)) / np.imag(sigma0**limit)
+        return q_inv
+
+    @classmethod
+    def two_level_systems(cls, params, temperatures, powers):
+        if powers is None or temperatures is None:
+            return 0.
+        # unpack parameters
+        f0 = params['f0'].value
+        fd = params['fd'].value
+        pc = params['pc'].value
+        # calculate Qinv
+        q_inv = fd
+        if temperatures is not None:
+            xi = h * f0 / (2 * kb * temperatures)
+            q_inv *= np.tanh(xi)
+        if powers is not None:
+            q_inv /= np.sqrt(1.0 + 10**((powers - pc) / 10.0))
+        return q_inv
+
+    @classmethod
+    def constant_loss(cls, params):
+        return params['q0_inv'].value
+
+    @classmethod
+    def model(cls, params, temperatures=None, powers=None, low_energy=False, parallel=False):
+        q_inv = cls.mattis_bardeen(params, temperatures, low_energy=low_energy, parallel=parallel)
+        q_inv += cls.two_level_systems(params, temperatures, powers)
+        q_inv += cls.constant_loss(params)
+        return 1 / q_inv
+
+    @classmethod
+    def residual(cls, params, data, temperatures=None, powers=None, sigmas=None, low_energy=False, parallel=False):
+        q = cls.model(params, temperatures=temperatures, powers=powers, low_energy=low_energy, parallel=parallel)
+        residual = (q - data) / sigmas if sigmas is not None else (q - data)
+        return residual
+
+    @classmethod
+    def guess(cls, data, f0, tc, alpha=0.5, bcs=BCS, temperatures=None, powers=None, fit_resonance=False,
+              fit_mbd=True, fix_tc=True, fit_alpha=True, fit_tls=True, high_power=False, fit_loss=True,
+              fit_dynes=False, limit=-1):
+        scale = 2 if fit_tls and fit_loss else 1
+        # guess constant loss
+        qi_inv_min = np.min(1 / data)
+        q0_inv = qi_inv_min / scale if fit_loss else 0
+        # guess two level system values
+        if temperatures is not None:
+            xi = h * f0 / (2 * kb * temperatures)
+            fd = np.min(qi_inv_min / np.tanh(xi)) / scale if fit_tls else 0
+        else:
+            fd = 0
+        pc = np.mean(powers) if powers is not None else 0
+        gamma_sqrt = 0.1 if fit_dynes is True else np.sqrt(fit_dynes)
+
+        # make the parameters object (coerce all values to float to avoid ints and numpy types)
+        params = lm.Parameters(usersyms={'scaled_alpha_inv': scaled_alpha_inv})
+        # resonator params
+        params.add("f0", value=float(f0), vary=fit_resonance, min=0)
+        params.add("limit", value=float(limit), vary=False)
+        # constant loss parameters
+        params.add("q0_inv", value=float(q0_inv), vary=fit_loss)
+        # two level system params
+        params.add("fd_scaled", value=float(np.sqrt(fd * 1e6)), vary=fit_tls)
+        params.add("pc", value=float(pc if high_power else np.inf), vary=fit_tls and high_power)
+        # Mattis-Bardeen params
+        params.add("tc", value=float(tc), vary=fit_mbd and not fix_tc, min=0)
+        params.add("bcs", value=float(bcs), vary=fit_mbd and fix_tc, min=0)
+        params.add("scaled_alpha", value=float(scaled_alpha(alpha)), vary=fit_mbd and fit_alpha)
+        # Dynes params
+        params.add("gamma_sqrt", value=float(gamma_sqrt), vary=bool(fit_dynes))
+        # derived params
+        params.add("alpha", expr='scaled_alpha_inv(scaled_alpha)')
+        params.add("fd", expr='fd_scaled**2 * 1e-6')
+        params.add("gamma", expr="gamma_sqrt**2")
+
+        return params
