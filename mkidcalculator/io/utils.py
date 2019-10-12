8ecfb069df7f003392bf9668cb4f31b7c47135d6
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-10-11 17:53:31 -0700

    Added sweep functions previously in sweep_fitting to mkidcalculator.io.utils.py since they will be used by the Sweep class

--- mkidcalculator/io/utils.py
+++ mkidcalculator/io/utils.py
@@ -4,8 +4,9 @@ import logging
 import tempfile
 import numpy as np
 import lmfit as lm
-import scipy.constants as c
 from collections import OrderedDict
+import scipy.constants as c
+from scipy.signal import find_peaks, detrend
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -361,3 +362,331 @@ def get_plot_model(self, fit_type, label, params=None, calibrate=False, default_
     if plot_kwargs is not None:
         kwargs.update(plot_kwargs)
     return f, m, kwargs
+
+
+def _integer_bandwidth(f, df):
+    return int(np.round(df / (f[1] - f[0]) / 2) * 2)  # nearest even number
+
+
+def find_resonators(f, z, df, **kwargs):
+    """
+    Find resonators in a S21 trace.
+    Args:
+        f: numpy.ndarray
+            The frequencies corresponding to the magnitude array.
+        z: numpy.ndarray
+            The complex scattering data.
+        df: float
+            The frequency bandwidth for each resonator. df / 2 will be used as
+            the max peak width unless overridden.
+        kwargs: optional keyword arguments
+            Optional keyword arguments to scipy.signal.find_peaks. Values here
+            will override the defaults.
+    Returns:
+        peaks: numpy.ndarray, dtype=integer
+            An array of peak integers
+    """
+    # resonator bandwidth in indices
+    dfii = _integer_bandwidth(f, df)
+    # detrend magnitude data for peak finding
+    magnitude = detrend(20 * np.log10(np.abs(z)))
+    fit = np.argsort(magnitude)[:int(3 * len(magnitude) / 4):-1]
+    poly = np.polyfit(f[fit], magnitude[fit], 1)
+    magnitude = magnitude - np.polyval(poly, f)
+    # find peaks
+    kws = {"prominence": 1, "height": 5, "width": (None, int(dfii / 2))}
+    kws.update(kwargs)
+    peaks, _ = find_peaks(-magnitude, **kws)
+    # cut out resonators that are separated from neighbors by less than df / 2
+    right = np.hstack((np.diff(f[peaks]) > df / 2, False))
+    left = np.hstack((False, np.diff(f[peaks][::-1])[::-1] < -df / 2))
+    logic = left & right
+    peaks = peaks[logic]
+    return peaks
+
+
+def collect_resonances(f, z, peaks, df):
+    """
+    Collect all of the resonances from a data into an array.
+    Args:
+        f: numpy.ndarray
+            The frequencies corresponding to i and q.
+        z: numpy.ndarray
+            The S21 complex scattering data.
+        peaks: numpy.ndarray, dtype=integer
+            The indices corresponding to the resonator locations.
+        df: float
+            The final bandwidth of all of the outputs.
+    Returns:
+        f_array: numpy.ndarray
+            A MxN array for the frequencies where M is the number of resonators
+            and N is the number of frequencies.
+        z_array: numpy.ndarray
+            A MxN array for the S21 data where M is the number of resonators
+            and N is the number of frequencies.
+        peaks: numpy.ndarray, dtype=integer
+            The peak indices corresponding to resonator locations. Some indices
+            may be removed due to encroaching nearby resonators.
+    """
+    # resonator bandwidth in indices
+    dfii = _integer_bandwidth(f, df)
+    # collect resonance data into arrays
+    f_array = np.empty((len(peaks), int(dfii)))
+    z_array = np.empty(f_array.shape, dtype=np.complex)
+    for ii in range(f_array.shape[0]):
+        f_array[ii, :] = f[int(peaks[ii] - dfii / 2): int(peaks[ii] + dfii / 2)]
+        z_array[ii, :] = z[int(peaks[ii] - dfii / 2): int(peaks[ii] + dfii / 2)]
+    # cut out resonators that aren't centered (large resonator tails on either side)
+    logic = np.abs(np.argmin(np.abs(z_array), axis=-1) - dfii / 2) > dfii / 10
+    return f_array[logic, :], z_array[logic, :], peaks[logic]
+
+
+def get_loop_fit_info(loops, label='best', parameters=("fr", "qi", "q0", "chi2"), bounds=None, errorbars=None,
+                      success=None):
+    """
+    Get fit information from a list of Loops
+    Args:
+        loops: list of mkidcalculator.Loop objects
+            The fitted loops to extract the information from.
+        label: string (optional)
+            The fit label to use.
+        parameters: tuple of strings
+            The fit parameters to report. "chi2" can be used to retrieve
+            the reduced chi squared values.
+        bounds: tuple of numbers or tuples
+            The bounds for the parameters. It must be a tuple of the same
+            length as the parameters keyword argument. Each element is either
+            an upper bound on the parameter or a tuple, e.g. (lower bound,
+            upper bound). None can be used as a placeholder to skip a
+            parameter. The default is None and no bounds are used.
+        errorbars: boolean
+            If errorbars is True, only data from loop fits that could compute
+            errorbars on the fit parameters is included. If errorbars is False,
+            only data from loop fits that could not compute errorbars on the
+            fit parameters is included. The default is None, and no filtering
+            on the errorbars is done.
+        success: boolean
+            If success is True, only data from successful loop fits is
+            included. If False, only data from failed loop fits is
+            included. The default is None, and no filtering on fit success is
+            done. Note: fit success is typically a bad indicator on fit
+            quality. It only ever fails when something really bad happens.
+    Returns:
+        outputs: tuple of numpy.ndarray objects
+            The outputs in the same order as parameters.
+    """
+    outputs = []
+    for parameter in parameters:
+        outputs.append([])
+        for loop in loops:
+            result = loop.lmfit_results[label]['result']
+            if errorbars is not None and result.errorbars != errorbars:
+                continue  # skip if wrong errorbars setting
+            if success is not None and result.success != success:
+                continue  # skip if wrong success setting
+            if parameter == "chi2":
+                outputs[-1].append(result.redchi)
+            else:
+                outputs[-1].append(result.params[parameter].value)
+    # turn outputs into a list of numpy arrays
+    for index, output in enumerate(outputs):
+        outputs[index] = np.array(output)
+    # format bounds if None
+    if bounds is None:
+        bounds = [None] * len(outputs)
+    # make filtering logic
+    logic = np.ones(outputs[-1].shape, dtype=bool)
+    for index, output in enumerate(outputs):
+        if bounds[index] is None:
+            continue
+        elif isinstance(bounds[index], (list, tuple)):
+            logic = logic & (output >= bounds[index][0]) & (output <= bounds[index][1])
+        else:
+            logic = logic & (output <= bounds[index])
+    # filter outputs
+    for index, output in enumerate(outputs):
+        outputs[index] = output[logic]
+    return tuple(outputs)
+
+
+def plot_parameter_hist(parameter, title=None, x_label=True, y_label=True, label_kwargs=None, tick_kwargs=None,
+                        tighten=True, return_bin=False, axes=None, **kwargs):
+    """
+    Plot a parameter histogram.
+    Args:
+        parameter: numpy.ndarray
+            An array of parameter values.
+        title: string (optional)
+            The title for the plot. The default is None and no title is made.
+        x_label: string, boolean (optional)
+            The x label for the plot. The default is True and the default label
+            is used. If False, no label is used.
+        y_label: string, boolean (optional)
+            The y label for the plot. The default is True and the default label
+            is used. If False, no label is used.
+        label_kwargs: dictionary
+            Keyword arguments for the axes labels in axes.set_*label(). The
+            default is None which uses default options. Keywords in this
+            dictionary override the default options.
+        tick_kwargs: dictionary
+            Keyword arguments for the ticks using axes.tick_params(). The
+            default is None which uses the default options. Keywords in
+            this dictionary override the default options.
+        tighten: boolean (optional)
+            Whether or not to apply figure.tight_layout() at the end of the
+            plot. The default is True.
+        return_bin: boolean (optional)
+            Whether or not to include the binned information in the returned
+            values. The default is False and only the axes are returned.
+        axes: matplotlib.axes.Axes class
+            An Axes class on which to put the plot. The default is None and
+            a new figure is made.
+        kwargs: optional keyword arguments
+            Extra keyword arguments to send to axes.hist().
+        Returns:
+            axes: matplotlib.axes.Axes class
+                An Axes class with the plotted data.
+            centers: numpy.ndarray
+                The histogram bin centers. Only returned if return_bin is True.
+            counts: numpy.ndarray
+                The histogram bin counts. Only returned if return_bin is True.
+    """
+    if axes is None:
+        from matplotlib import pyplot as plt
+        figure, axes = plt.subplots()
+    else:
+        figure = axes.figure
+    kws = {"bins": 50}
+    if kwargs:
+        kws.update(kwargs)
+    counts, edges, _ = axes.hist(parameter, **kws)
+    axes.set_xlim(left=0)
+    kws = {}
+    if label_kwargs is not None:
+        kws.update(label_kwargs)
+    if x_label is not False:
+        axes.set_xlabel("parameter values" if x_label is True else x_label, **kws)
+    if y_label is not False:
+        axes.set_ylabel("counts per bin" if y_label is True else y_label, **kws)
+    if tick_kwargs is not None:
+        axes.tick_params(**tick_kwargs)
+    if title is not False and title is not None:
+        axes.set_title(title)
+    if tighten:
+        figure.tight_layout()
+    if return_bin:
+        centers = 0.5 * (edges[1:] + edges[:-1])
+        return axes, centers, counts
+    return axes
+
+
+def plot_parameter_vs_f(parameter, f, title=None, x_label=True, y_label=True, label_kwargs=None, tick_kwargs=None,
+                        tighten=True, scatter=True, median=True, bins=30, extend=True, return_bin=False, axes=None,
+                        median_kwargs=None, scatter_kwargs=None):
+    """
+    Plot a parameter vs frequency.
+    Args:
+        parameter: numpy.ndarray
+            An array of parameter values.
+        f: numpy.ndarray
+            The frequencies corresponding to the parameter values.
+        title: string (optional)
+            The title for the plot. The default is None and no title is made.
+        x_label: string, boolean (optional)
+            The x label for the plot. The default is True and the default label
+            is used. If False, no label is used.
+        y_label: string, boolean (optional)
+            The y label for the plot. The default is True and the default label
+            is used. If False, no label is used.
+        label_kwargs: dictionary
+            Keyword arguments for the axes labels in axes.set_*label(). The
+            default is None which uses default options. Keywords in this
+            dictionary override the default options.
+        tick_kwargs: dictionary
+            Keyword arguments for the ticks using axes.tick_params(). The
+            default is None which uses the default options. Keywords in
+            this dictionary override the default options.
+        tighten: boolean (optional)
+            Whether or not to apply figure.tight_layout() at the end of the
+            plot. The default is True.
+        scatter: boolean (optional)
+            Whether to plot a scatter of the data as a function of frequency.
+        median: boolean (optional)
+            Whether to plot the median as a function of frequency.
+        bins: integer (optional)
+            The number of bins to use in the median plot. The default is 30.
+        extend: boolean (optional)
+            Determines whether or not to extend the median data so that there
+            is a bin with zero values on either side of the frequency range.
+            The default is True.
+        return_bin: boolean (optional)
+            Whether or not to include the binned median information in the
+            returned values. The default is False and only the axes are
+            returned. The bin values are not returned if median is False.
+        axes: matplotlib.axes.Axes class
+            An Axes class on which to put the plot. The default is None and
+            a new figure is made.
+        median_kwargs: dictionary (optional)
+            Extra keyword arguments to send to axes.step().
+        scatter_kwargs: dictionary (optional)
+            Extra keyword arguments to send to axes.plot().
+        Returns:
+            axes: matplotlib.axes.Axes class
+                An Axes class with the plotted data.
+            centers: numpy.ndarray
+                The bin centers. Only returned if return_bin and median are
+                True.
+            medians: numpy.ndarray
+                The median values in each bin. Only returned if return_bin and
+                median are True.
+    """
+    if axes is None:
+        from matplotlib import pyplot as plt
+        figure, axes = plt.subplots()
+    else:
+        figure = axes.figure
+
+    if scatter:
+        kws = {"linestyle": "none"}
+        if scatter_kwargs is not None:
+            kws.update(scatter_kwargs)
+        axes.plot(f, parameter)
+    if median:
+        edges = np.linspace(f.min(), f.max(), bins)
+        centers = 0.5 * (edges[1:] + edges[:-1])
+        medians = np.empty(bins - 1)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)  # median of empty slice
+            for ii in range(len(medians) - 1):
+                medians[ii] = np.median(parameter[(f >= edges[ii]) & (f < edges[ii + 1])])
+            medians[-1] = np.median(parameter[(f >= edges[-2]) & (f <= edges[-1])])  # last bin is fully closed
+        medians[np.isnan(medians)] = 0
+        if extend:
+            dx = centers[1] - centers[0]
+            centers = np.hstack([centers[0] - dx, centers, centers[-1] + dx])
+            medians = np.hstack([0, medians, 0])
+        kws = {"where": "mid"}
+        if median_kwargs is not None:
+            kws.update(median_kwargs)
+        axes.step(centers, medians, **kws)
+    else:
+        centers = None
+        medians = None
+    axes.set_xlim(f.min(), f.max())
+    axes.set_ylim(bottom=0)
+    kws = {}
+    if label_kwargs is not None:
+        kws.update(label_kwargs)
+    if x_label is not False:
+        axes.set_xlabel("frequency [GHz]" if x_label is True else x_label, **kws)
+    if y_label is not False:
+        axes.set_ylabel("median parameter" if y_label is True else y_label, **kws)
+    if tick_kwargs is not None:
+        axes.tick_params(**tick_kwargs)
+    if title and title is not None:
+        axes.set_title(title)
+    if tighten:
+        figure.tight_layout()
+    if return_bin and median:
+        return axes, centers, medians
+    return axes
