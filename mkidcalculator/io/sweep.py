ffffbcca42802cf621465aee5e8a3b41ca8e827c
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-10-11 17:54:34 -0700

    Sweep summary plotting

--- mkidcalculator/io/sweep.py
+++ mkidcalculator/io/sweep.py
@@ -2,9 +2,11 @@ import os
 import pickle
 import logging
 
+from mkidcalculator.io.loop import Loop
 from mkidcalculator.io.resonator import Resonator
-from mkidcalculator.io.data import analogreadout_sweep
+from mkidcalculator.io.data import analogreadout_sweep, mazinlab_widesweep
 from mkidcalculator.io.utils import (find_resonators, collect_resonances, get_loop_fit_info, plot_parameter_vs_f,
+                                     plot_parameter_hist)
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -158,3 +160,91 @@ class Sweep:
         self._directory = directory
         for resonator in self.resonators:
             resonator._set_directory(self._directory)
+
+    def plot_loop_fits(self, parameters=("qi", "q0", "chi2"), bounds=None, errorbars=True, success=True,
+                       title=True, tighten=True, label='best', plot_kwargs=None, figure=None):
+        """
+        Plot a summary of all the loop fits.
+        Args:
+            parameters: tuple of strings
+                The fit parameters to plot. "chi2" can be used to plot the
+                reduced chi squared value.
+            bounds: tuple of numbers or tuples
+                The bounds for the parameters. It must be a tuple of the same
+                length as the parameters keyword argument. Each element is either
+                an upper bound on the parameter or a tuple, e.g. (lower bound,
+                upper bound). None can be used as a placeholder to skip a
+                bound. The default is None and no bounds are used.
+            errorbars: boolean
+                If errorbars is True, only data from loop fits that could compute
+                errorbars on the fit parameters is included. If errorbars is False,
+                only data from loop fits that could not compute errorbars on the
+                fit parameters is included. The default is True. None may be used
+                to enforce no filtering on the errorbars.
+            success: boolean
+                If success is True, only data from successful loop fits is
+                included. If False, only data from failed loop fits is
+                included. The default is True. None may be used
+                to enforce no filtering on success. Note: fit success is typically
+                a bad indicator on fit quality. It only ever fails when something
+                really bad happens.
+            title: string or boolean (optional)
+                The title to use for the summary plot. The default is True and the
+                default title will be applied. If False, no title is applied.
+            tighten: boolean (optional)
+                Whether or not to apply figure.tight_layout() at the end of the
+                plot. The default is True.
+            label: string (optional)
+                The fit label to use for the plots. The default is 'best'.
+            plot_kwargs: dictionary or list of dictionaries (optional)
+                A dictionary or list of dictionaries containing plot options. If
+                only one is provided, it is used for all of the plots. If a list
+                is provided, it must be of the same length as the number of plots.
+                No kwargs are passed by default.
+            figure: matplotlib.figure.Figure class
+                A figure to use for the plots. The default is None and one is
+                created automatically.
+        Returns:
+            figure: matplotlib.figure.Figure class
+                The figure object for the plot.
+        """
+        # get the data
+        loops = []
+        for resonator in self.resonators:
+            loops += resonator.loops
+        parameters = ["fr"] + list(parameters)
+        outputs = get_loop_fit_info(loops, label=label, parameters=parameters, bounds=bounds, success=success,
+                                    errorbars=errorbars)
+        # create figure if needed
+        if figure is None:
+            from matplotlib import pyplot as plt
+            figure = plt.figure(figsize=(8.5, 11))
+        # setup figure axes
+        gs = gridspec.GridSpec(len(parameters) - 1, 2)
+        axes_list = np.array([figure.add_subplot(gs_ii) for _, gs_ii in np.ndenumerate(gs)])
+        # check plot kwargs
+        if plot_kwargs is None:
+            plot_kwargs = {}
+        if isinstance(plot_kwargs, dict):
+            plot_kwargs = [plot_kwargs] * len(axes_list)
+        # add plots
+        for index in range(axes_list // 2):
+            kws = {"x_label": parameters[index + 1]}
+            if plot_kwargs[0]:
+                kws.update(plot_kwargs[2 * index])
+            plot_parameter_hist(outputs[index + 1], axes=axes_list[0], **kws)
+            kws = {"y_label": "median " + parameters[index + 1]}
+            if plot_kwargs[1]:
+                kws.update(plot_kwargs[2 * index + 1])
+            plot_parameter_vs_f(outputs[index + 1], outputs[0], axes=axes_list[1], **kws)
+        # add title
+        if title:
+            title = "sweep fit summary: '{}'".format(label) if title is True else title
+            figure.suptitle(title, fontsize=15)
+            rect = [0, 0, 1, .95]
+        else:
+            rect = [0, 0, 1, 1]
+        # tighten
+        if tighten:
+            figure.tight_layout(rect=rect)
+        return figure
