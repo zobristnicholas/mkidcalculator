62862aec448d113ddbff05ae4a1ec08fccb3d32c
nzobrist <zobrist.nicholas@mac.com>
2021-06-07 16:42:10 -0700

    add outlier filtering to Tc measurement

--- mkidcalculator/experiments/tc_test.py
+++ mkidcalculator/experiments/tc_test.py
@@ -1,17 +1,19 @@
 import calendar
 import numpy as np
 from datetime import datetime
+from scipy.signal import detrend
 from scipy.interpolate import interp1d
+from scipy.stats import median_abs_deviation
 from mkidcalculator.io.utils import setup_axes, finalize_axes
 
 
 def load_lakeshore_log(filename, start=None, stop=None, timestamp=False,
-                       absolute=True):
+                       absolute=True, remove_outliers=False, nsigma=None):
     """
     Load the log file and return the times and values.
     Args:
-        filename: string
-            The log file name including the path.
+        filename: string or list of strings
+            The log filename(s) including the path.
         start: datetime.datetime
             The start time for the experiment. If not provided, the whole
             log file is used.
@@ -24,6 +26,13 @@ def load_lakeshore_log(filename, start=None, stop=None, timestamp=False,
         absolute: boolean
             The default is True and the absolute value of the data is
             returned. If False, the original data is returned instead.
+        remove_outliers: boolean
+            Remove outliers from the data if True. The default is False.
+        nsigma: float
+            If 'remove_outliers' is True, specifying 'nsigma' will mask
+            the detrended data outside nsigma. If 'remove_outliers' is
+            True, but 'nsigma' is not specified only extremely bad values
+            are removed.
     Returns:
         time: numpy.ndarray
             Times corresponding to the values.
@@ -31,7 +40,13 @@ def load_lakeshore_log(filename, start=None, stop=None, timestamp=False,
             Values at the corresponding times.
     """
     # Load the file.
-    data = np.loadtxt(filename, dtype=np.object, delimiter=",")
+    if isinstance(filename, str):
+        filename = [filename]
+    data = []
+    for name in filename:
+        data.append(np.loadtxt(name, dtype=np.object, delimiter=","))
+    data = np.vstack(data)
+
     # Reformat the time into a datetime.
     time_strings = [t[0].strip() + " " + t[1].strip() for t in data]
     fmt = "%d-%m-%y %H:%M:%S"
@@ -49,26 +64,46 @@ def load_lakeshore_log(filename, start=None, stop=None, timestamp=False,
     # Convert times to timestamps for easy manipulation.
     if timestamp:
         time = np.array([t.timestamp() for t in time])
+    # Remove outliers.
+    if remove_outliers:
+        # Get rid of values greater than nsigma from the detrended data.
+        if nsigma is not None:
+            v = detrend(values)
+            mask = np.abs(v) < nsigma * median_abs_deviation(v)
+        # Get rid of large values of the opposite sign w.r.t the measurement.
+        else:
+            sign = np.sign(np.median(values))
+            v_max = np.max(np.abs(values[np.sign(values) == sign]))
+            mask = np.abs(values) < v_max
+        values = values[mask]
+        time = time[mask]
     # Return absolute values.
     if absolute:
         values = np.abs(values)
     return time, values
 
 
-def t_vs_r(t_filename, r_filename, start=None, stop=None):
+def t_vs_r(t_filename, r_filename, start=None, stop=None, t_kwargs=None,
+           r_kwargs=None):
     """
     Return the resistance as a function of temperature.
     Args:
-        t_filename: string
-            The filename for the temperature log data.
-        r_filename: string
-            The file name for the resistance log data.
+        t_filename: string or list of strings
+            The filename(s) for the temperature log data.
+        r_filename: string or list of strings
+            The filename(s) for the resistance log data.
         start: datetime.datetime
             The start time for the experiment. If not provided, the whole
             log file is used.
         stop: datetime.datetime
             The stop time for the experiment. If not provided, the whole
             log file is used.
+        t_kwargs: dictionary
+            Keyword arguments to use when loading the temperature with
+            the load_lakeshore_log() function.
+        r_kwargs: dictionary
+            Keyword arguments to use when loading the resistance with
+            the load_lakeshore_log() function.
     Returns:
         temperature: numpy.ndarray
             Temperature at the same time as the resistance.
@@ -76,34 +111,33 @@ def t_vs_r(t_filename, r_filename, start=None, stop=None):
             Resistance at the same time as the temperature.
     """
     # Load the files.
+    kwargs = {'remove_outliers': True, 'nsigma': 10}
+    if t_kwargs is not None:
+        kwargs.update(t_kwargs)
     time_t, t = load_lakeshore_log(t_filename, start=start, stop=stop,
-                                   timestamp=True)
-    time_r, resistance = load_lakeshore_log(r_filename, start=start,
-                                            stop=stop, timestamp=True)
+                                   timestamp=True, **kwargs)
+    kwargs = {'remove_outliers': True}
+    if r_kwargs is not None:
+        kwargs.update(r_kwargs)
+    time_r, resistance = load_lakeshore_log(r_filename, start=start, stop=stop,
+                                            timestamp=True, **kwargs)
     # Get the temperature when the resistance was measured by interpolation.
     time_to_t = interp1d(time_t, t, fill_value='extrapolate')
     temperature = time_to_t(time_r)
     return temperature, resistance
 
 
-def plot_transition(t_filename, r_filename, start=None, stop=None,
-                    plot_kwargs=None, x_label=None, y_label=None,
-                    label_kwargs=None, legend=False, legend_kwargs=None,
-                    title=False, title_kwargs=None, tick_kwargs=None,
-                    tighten=True, axes=None):
+def plot_transition(t_filename, r_filename, plot_kwargs=None, x_label=None,
+                    y_label=None, label_kwargs=None, legend=False,
+                    legend_kwargs=None, title=False, title_kwargs=None,
+                    tick_kwargs=None, tighten=True, axes=None, **kwargs):
     """
     Plot the transition data.
     Args:
-        t_filename: string
-            The filename for the temperature log data.
-        r_filename: string
-            The file name for the resistance log data.
-        start: datetime.datetime
-            The start time for the experiment. If not provided, the whole
-            log file is used.
-        stop: datetime.datetime
-            The stop time for the experiment. If not provided, the whole
-            log file is used.
+        t_filename: string or list of strings
+            The filename(s) for the temperature log data.
+        r_filename: string or list of strings
+            The file name(s) for the resistance log data.
         x_label: string
             The label for the x axis. The default is None which uses the
             default label. If x_label evaluates to False, parameter_kwargs
@@ -145,11 +179,13 @@ def plot_transition(t_filename, r_filename, start=None, stop=None,
         axes: matplotlib.axes.Axes class
             An Axes class on which to put the plot. The default is None and
             a new figure is made.
+        kwargs: optional keyword arguments
+            These keyword arguments are set to the t_vs_r() function.
     Returns:
         axes: matplotlib.axes.Axes class
             An Axes class with the plotted loop.
     """
-    t, r = t_vs_r(t_filename, r_filename, start=start, stop=stop)
+    t, r = t_vs_r(t_filename, r_filename, **kwargs)
     _, axes = setup_axes(axes, x_label, y_label, label_kwargs,
                          'T [mK]', r'R [$\Omega$]')
     kwargs = {"marker": 'o', "markersize": 4, "linestyle": "none",
